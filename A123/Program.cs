d88e132494d077f3abb518d16030a1a97f0c17e9
cocolin <cocolin921118@gmail.com>
2024-08-23 10:35:13 +0800

    第一次測試成功

---
+++ A123/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace A123
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("嘿嘿"); 
+        }
+    }
+}
